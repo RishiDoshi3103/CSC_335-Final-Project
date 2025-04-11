c2133a55a99de18a11e3b32df17d3b370c5df79e
Kyle Becker <kabecker@Save-the-Queen.local>
2025-04-11 11:19:17 -0700

    Temporary Score reference added to old controller, can add to Game_Controller.java when scoring becomes relevant

--- Cribbage_Game/src/controller/GameController.java
+++ Cribbage_Game/src/controller/GameController.java
@@ -1,5 +1,49 @@
 package controller;
 
+/**
+ * 
+ *  Score Reference
+ *  Scoring is seemingly broken into four different phases: the pre-play that accounts
+ *  for only the start card, play where cards are considered at each turn, the show at
+ *  the end for full hand analysis, and the crib at the end for the dealer. Dealer 
+ *  switches off each round.
+ *  
+ *  Pre-Play:
+ *   - If the start card is a Jack, the dealer gets............................1 point
+ *   
+ *  During Play: 
+ *   - If one player says "go", the other player gets..........................1 point
+ * 	 - Playing the last card of a round that doesn't go over 31................1 point
+ *   - Playing a card that causes the total to equal 15 or 31..................2 points
+ *   - Playing the same RANK as the last played card (pair)....................2 points
+ *   - Playing a third same RANK card as the last two (pair royal).............6 points
+ *   - Playing a fourth same RANK card as the last three (Double pair royal)...12 points
+ *   - Playing the third (or more) card of a consecutive run of RANKs 
+ *     (i.e, 2♡, 3♧, 4♤.. etc), grants points equal to the total cards in
+ *     that run (3 consecutive cards gives 3 points. 4 grants 4 points, etc)...3+ points
+ *     ** Note: they don't have to be in direct order (2, 4, 3 counts, so 
+ *     long as there isn't another number in between them that wouldn't  
+ *     qualify: 2, 8, 4, 3 doesn't count) **
+ *   
+ *   The show: (Scores based on individual hands and start card - includes dealer)
+ *   - Any combination of cards adding up to 15 (can reuse cards)..............2 points
+ *   - A pair (two of the same RANK)...........................................2 points
+ *   - Three of a kind (three of the same RANK)................................6 points
+ *   - Four of a kind (four of the same RANK)..................................12 points
+ *   - The longest consecutive run of RANKS (similar to play) grants
+ *     points equal to the total cards in that run.............................3+ points
+ *     ** Note: smaller runs in a bigger run do not count, only the longest
+ *     consecutive streak is counted.
+ *   - If all 4 cards in hand are same SUIT (Flush)............................4 points
+ *   - If all 4 cards in hand AND the start card are same SUIT (Flush).........5 points
+ *   - If a Jack is in players hand and is same SUIT as start card.............1 point
+ *   
+ *   The Crib:
+ *   - Same as the show, except can only get a flush if all 4 in the crib, and the
+ *   start cards are same SUIT.
+ */
+
+
 public class GameController {
 
 }
