fc39c3bf1a394f11fc6d74be040af041df6e0208
liammnorris <148369713+liammnorris@users.noreply.github.com>
2025-04-23 14:30:45 -0700

    Added functionality for choosing game type

--- Cribbage_Game/src/main/Main.java
+++ Cribbage_Game/src/main/Main.java
@@ -2,11 +2,23 @@ package main;
 
 import controller.GameController;
 import model.Game;
+import ui.GameStarter;
 import ui.TextViewer;
 
 public class Main {
 	public static void main(String[] args) {
-		Game model = new Game("Player1", "Player2");
+		GameStarter starter = new GameStarter();
+		int mode = starter.GameMode();
+		Game model;
+		if (mode == 1) {
+			model = new Game("Player1", "Player2", 0);
+		}
+		else if (mode == 2) {
+			model = new Game("Player1", "Player2", 1);
+		}
+		else {
+			model = new Game("Player1", "Player2", 2);
+		}
 		TextViewer view = new TextViewer();
 		GameController controller = new GameController(model, view);
 		controller.playCribbage();
