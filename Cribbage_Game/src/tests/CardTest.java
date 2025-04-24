5bde79a4f02051f2677feb004e162e4cfef08192
Kyle Becker <kabecker@Save-the-Queen.local>
2025-04-24 16:33:28 -0700

    Test Update - need to review Card before submission

--- Cribbage_Game/src/tests/CardTest.java
+++ Cribbage_Game/src/tests/CardTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import model.Card;
+import model.Rank;
+import model.Suit;
 
 /**
  * Unit tests for the Card class.
@@ -16,18 +18,52 @@ public class CardTest {
      */
     @Test
     public void testCardProperties() {
-        Card cardA = new Card("A", "Spades");
-        assertEquals("A", cardA.getRank());
-        assertEquals("Spades", cardA.getSuit());
-        assertEquals(1, cardA.getCribbageValue());
-        assertEquals(1, cardA.getRankValue());
+       Card card1 = Card.get(Suit.DIAMONDS, Rank.FIVE);
+       Card card2 = Card.get(Suit.DIAMONDS, Rank.FIVE);
+       Card card3 = Card.get(Suit.HEARTS, Rank.TWO);
+       Card card4 = Card.get(Suit.SPADES, Rank.KING);
+       Card card5 = Card.get(Suit.CLUBS, Rank.ACE);
+       
+       assertSame(card1, card2);
+       
+       assertEquals(card1.getRank(), Rank.FIVE);
+       assertEquals(card2.getSuit(), Suit.DIAMONDS);
+       assertEquals(card1.toString(), "FIVE\u2666");
+       assertEquals(card3.toString(), "TWO\u2665");
+       assertEquals(card4.toString(), "KING\u2660");
+       assertEquals(card5.toString(), "ACE\u2663");
+       
+       assertThrows(AssertionError.class, () -> Card.get(null, Rank.ACE));
+       assertThrows(AssertionError.class, () -> Card.get(Suit.DIAMONDS, null));
 
-        Card cardTen = new Card("10", "Hearts");
-        assertEquals(10, cardTen.getCribbageValue());
-        assertEquals(10, cardTen.getRankValue());
+       //assertTrue(card1.compare(card2, card3) > 0 );
+       
+       // -- Testing Rank isSequential --
+       assertEquals(card1.getRank().isSequential(Rank.SIX), 1);
+       assertEquals(card1.getRank().isSequential(Rank.FOUR), -1);
+       assertEquals(card1.getRank().isSequential(Rank.FIVE), 0);
+       assertEquals(card1.getRank().isSequential(Rank.SEVEN), 0);
+       
+       assertEquals(Rank.NINE.isSequential(Rank.EIGHT), -1);
+       assertEquals(Rank.NINE.isSequential(Rank.TEN), 1 );
+       assertEquals(Rank.NINE.isSequential(Rank.SEVEN), 0);
+       
+       assertEquals(Rank.TEN.isSequential(Rank.NINE), -1);
+       assertEquals(Rank.TEN.isSequential(Rank.JACK), 1);
+       assertEquals(Rank.TEN.isSequential(Rank.ACE), 0);
+       
+       assertEquals(Rank.JACK.isSequential(Rank.TEN), -1);
+       assertEquals(Rank.JACK.isSequential(Rank.QUEEN), 1);
+       assertEquals(Rank.JACK.isSequential(Rank.TWO), 0);
+       
+       assertEquals(Rank.QUEEN.isSequential(Rank.JACK), -1);
+       assertEquals(Rank.QUEEN.isSequential(Rank.KING), 1);
+       assertEquals(Rank.QUEEN.isSequential(Rank.FIVE), 0);
+       
+       assertEquals(Rank.KING.isSequential(Rank.QUEEN), -1);
+       assertEquals(Rank.KING.isSequential(Rank.KING), 0);
+       assertEquals(Rank.KING.isSequential(Rank.SEVEN), 0);
 
-        Card cardJ = new Card("J", "Diamonds");
-        assertEquals(10, cardJ.getCribbageValue());
-        assertEquals(11, cardJ.getRankValue());
     }
+    
 }
